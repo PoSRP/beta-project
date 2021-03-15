aa883d6d96431692ab3ac3f88870edd076270917
jacobXPS <Jawar19@student.sdu.dk>
2021-03-15 12:46:46 +0100

    plant model and params file

---
+++ matlab/InvertedPend_simscape.m
@@ -0,0 +1,24 @@
+%% Simscape sim of Cart
+clear
+
+%% parameters of system
+
+% Lengths
+    l_p     = 0.35;      %[m]
+    l_p_com = 0.1791;    %[m] pendulum center of mass
+    l_t     = 1.72;      %[m] track length 
+
+% damping coeff
+    b_c     = 5;         % [N/(m/s)]
+    b_p     = 0.0012;    % [N/(m/s)]
+
+% mass
+    m_c     = 0.5;       %[kg]
+    m_p     = 0.084;     %[kg]
+    
+% other forces
+    g       = 9.82;      %[m/s^2]
+
+%% run simscape
+
+open_system('semPro4/Pend_cart');
